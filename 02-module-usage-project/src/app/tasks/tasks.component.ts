f752522193347106fa03c167897537c5969236a1
Eder Nilson <eder.nilson@gmail.com>
2026-02-22 18:14:59 -0300

    Curso Angular - The Complete Guide

---
+++ 02-module-usage-project/src/app/tasks/tasks.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from "@angular/core";
+
+import { TasksService } from "./tasks.service";
+
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+
+@Component({
+  selector: "app-tasks",
+  standalone: true,
+  templateUrl: "./tasks.component.html",
+  styleUrl: "./tasks.component.css",
+  imports: [TaskComponent, NewTaskComponent],
+})
+export class TasksComponent {
+  @Input() user: any | undefined;
+  isAddingTask = false;
+
+  constructor(private tasksService: TasksService) {}
+
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.user.id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+
+}
