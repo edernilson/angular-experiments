859b7c0c4604c23ffdf68e8b39af3591b5697d5e
Eder Nilson <eder.nilson@gmail.com>
2026-02-24 12:11:09 -0300

    First Investiment Calculator version

---
+++ 03-investment-project/src/app/components/investiment-results/investiment-results.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investiment-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investiment-results.component.html',
+  styleUrl: './investiment-results.component.css'
+})
+export class InvestimentResultsComponent {
+
+}
---
+++ 03-investment-project/src/app/components/user-input/user-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }>();
+
+  enteredInitialInvestiment = '0';
+  enteredAnnualInvestiment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestiment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestiment,
+    });
+  }
+}
